bb30798d58fe0c3d21c175dbc25cf1f931234b1a
Yann Fanha <61124069+YFanha@users.noreply.github.com>
2021-03-04 16:01:24 +0100

    Ajout de la fonction d'enregistrement d'annonces

--- Code/src/controler/annonces.php
+++ Code/src/controler/annonces.php
@@ -1,7 +1,26 @@
 <?php
 
-function registerAnnonces(){
-    require 'view/formAnnonce.php';
+function registerAnnonces($newAnnonce){
+    if(isset($newAnnonce['inputAnnonceTitle']) &&
+        isset($newAnnonce['inputAnnoncePrice']) &&
+        isset($newAnnonce['inputAnnonceDescription']) &&
+        isset($newAnnonce['inputAnnoncePhoto'])){
+
+        $annonceTitle = $newAnnonce['inputAnnonceTitle'];
+        $annoncePrice = $newAnnonce['inputAnnoncePrice'];
+        $annonceDescription = $newAnnonce['inputAnnonceDescription'];
+        $annoncePhoto = $newAnnonce['inputAnnoncePhoto'];
+
+        require "model/annoncesManager.php";
+        $registerResult = registerNewAnnonce($annonceTitle, $annoncePrice, $annonceDescription, $annoncePhoto);
+        if ($registerResult){
+            require "view/home.php";
+        }else{
+            require "view/formAnnonce.php";
+        }
+    } else {
+        require "view/formAnnonce.php";
+    }
 }
 
 ?>
--- Code/src/model/annoncesManager.php
+++ Code/src/model/annoncesManager.php
@@ -1,3 +1,47 @@
 <?php
+function getAnnonces(){
+    $filename = "data/annonces.json";
+    $annonce =  json_decode(file_get_contents($filename),true);
+    return $annonce; //renvoi du tableau des annonces
+}
+
+function updateAnnonce($annonces){
+    $filename = "data/annonces.json";
+    file_put_contents($filename, json_encode($annonces, JSON_PRETTY_PRINT));
+}
+
+function registerNewAnnonce($annonceTitle, $annoncePrice, $annonceDescription, $annoncePhoto){
+    $result = false;
+
+    $annonces = getAnnonces();
+    $id = getNewAnnonceId($annonces);
+    $date = date("d.m.Y");
+
+    //add path to the photo
+    $path = "view\content\imgAnnonce\\";
+    $annoncePhoto = $path . $annoncePhoto;
+
+    //get the user id
+    $user_id = $_SESSION['id'];
+
+    $annonces[] = array('id'=>$id, 'annonceTitle'=>$annonceTitle, 'annonceDescription'=>$annonceDescription, 'annoncePrice'=>$annoncePrice, 'date'=>$date, 'user_id'=>$user_id, 'annoncePhoto'=>$annoncePhoto);
+
+    updateAnnonce($annonces);
+
+    return true;
+}
+
+function getNewAnnonceId($annonces){
+    $nbrAnnonces = count($annonces);
+
+    if($nbrAnnonces !== 0){
+        $lastId = $annonces[$nbrAnnonces-1]['id'];
+        $id = $lastId+1;
+    }else{
+        $id = 0;
+    }
+
+    return $id;
+}
 
 ?>
