c41a2752ef2cc996c00db554683bdbdfca6af5ad
Yann Fanha <61124069+YFanha@users.noreply.github.com>
2021-03-10 21:04:46 +0100

    ajout email dans l'affichae et j'ai mis une condition pour l'affichage des boutons
    condition = boutons présent si id correspond bien a celui de la session

--- Code/src/view/annonceDetaillee.php
+++ Code/src/view/annonceDetaillee.php
@@ -31,16 +31,15 @@ ob_start();
                 <h4><?=$annonce['annonceDescription']?></h4>
             </div>
             <div class="texte">
-                <h4><?=$annonce['date']?></h4>
+                <h4>Posté le <?=$annonce['date']?> par <i><a href="mailto:<?=$userEmail?>" class="link-info"><?=$userEmail?></a></i></h4>
             </div>
+            <?php if (isset($_SESSION['id']) && $_SESSION['id'] == $annonce['user_id']) :?>
             <div>
                 <br>
                 <a href="index.php?action=updateAnnonce&id=<?=$annonce['id']?>" class="btn btn-warning">Modifier</a>
                 <a href="index.php?action=deleteAnnonce&id=<?=$annonce['id']?>" class="btn btn-danger">Supprimer</a>
-                <?php
-                    //TODO AJOUTER EMAIL USER
-                ?>
             </div>
+            <?php endif; ?>
         </div>
         </div>
     </div>
