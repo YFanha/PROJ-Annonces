e134a49c79dd8c23ba485df39bb40a48fee1a7de
Yann Fanha <61124069+YFanha@users.noreply.github.com>
2021-03-11 09:21:39 +0100

    Changement style page des annonces

--- Code/src/view/affichageAnnonces.php
+++ Code/src/view/affichageAnnonces.php
@@ -19,45 +19,26 @@ ob_start();
     <h5><span style="color:red"><?= $deleteAnnonceErrorMessage; ?></span></h5>
 <?php endif ?>
 
-<div class="table-responsive">
-    <table class="table textcolor">
-        <tbody>
-        <!--<tr>
-            <th>Id</th>
-            <th>Titre</th>
-            <th>Description</th>
-            <th>Prix</th>
-            <th>Date d'inscription</th>
-            <th>Utilisateurs</th>
-            <th>Photo</th>
-        </tr>-->
+<!--col-12 col-xs-12 col-sm-6 col-md-4 col-lg-3-->
 
-        <?php foreach ($annonces as $annonce) : ?>
-                <?php $annonceId = $annonce['id']?>
-
-            <div class="card" style="width: 18rem;">
-                <img src="<?=$annonce['annoncePhoto']?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?=$annonce['annonceTitle']?></h5>
-                    <p class="card-text"><?=$annonce['annonceDescription']?></p>
-                    <p class="card-text"><?=$annonce['annonceCategorie']?></p>
-                    <a href="index.php?action=seeAnnonceDetails&id=<?=$annonceId?>" class="btn btn-primary">Voir annonce</a>
+<div class="container-fluid" id="center_annonce">
+    <div class="row" id="annonce-container">
+        <div class="grid-container">
+            <?php foreach ($annonces as $annonce) : ?>
+                    <?php $annonceId = $annonce['id']?>
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$annonce['annoncePhoto']?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?=$annonce['annonceTitle']?></h5>
+                        <p class="card-text"><?=$annonce['annonceDescription']?></p>
+                        <p class="card-text"><?=$annonce['annonceCategorie']?></p>
+                        <a href="index.php?action=seeAnnonceDetails&id=<?=$annonceId?>" class="btn btn-primary">Voir annonce</a>
+                    </div>
                 </div>
-            </div>
-
-            <!--<tr>
-                <td> <a href="index.php?action=seeAnnonceDetails&id=<?=$annonceId?>"> <?=$annonce['id']?></a> </td>
-                <td> <a href="index.php?action=seeAnnonceDetails&id=<?=$annonceId?>"> <?=$annonce['annonceTitle']?></a> </td>
-                <td class="description"><?=$annonce['annonceDescription']?></td>
-                <td><?=$annonce['annoncePrice']?></td>
-                <td><?=$annonce['date']?></td>
-                <td><?=$annonce['user_id']?></td>
-                <td><?=$annonce['annoncePhoto']?></td>
-            </tr>-->
-        <?php endforeach ?>
-        </tbody>
-    </table>
-
+            <?php endforeach ?>
+        </div>
+    </div>
+</div>
     <?php
     $content = ob_get_clean();
     require "gabarit.php";
