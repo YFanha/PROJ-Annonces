f3d4e22e9eabd1c41a1e9e7239cf7ff05b9d0b47
Yann Fanha <61124069+YFanha@users.noreply.github.com>
2021-03-22 11:19:44 +0100

    Correction d'un bug de la liste déroulante pour les services

--- Code/src/view/content/javaScript/formAnnonce.js
+++ Code/src/view/content/javaScript/formAnnonce.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const HIDDEN = "hide"; //class 'hide'
 
-    const selectCategorie = document.getElementById("categorie");
-    const selectServices =  document.getElementById("selectService");
+    const selectCategorie = document.getElementById("categorie"); //Liste déroulante des catégories
+    const selectServices =  document.getElementById("selectService"); //Liste déroulante des services
 
+    //Constante pour definir la valeur "Services" de la categorie
+    const SERVICES = "Services"
+
+    //Afficher la liste si la "services" est déjà choisi (lors de la modification)
+    if (selectCategorie.options[selectCategorie.selectedIndex].value === SERVICES){
+       selectServices.classList.remove(HIDDEN);
+    }
 
     //Faire apparaitre ou disparaitre le champ des types de services en fonction de la valeur du champ des catégorie
     selectCategorie.onchange = function () {
-        const SERVICES = "Services"
 
         let selectCategorieValue = selectCategorie.options[selectCategorie.selectedIndex].value;
 
