2d3fe47354bd98e01a4acdb49382d9116ad34ac3
Yann Fanha <61124069+YFanha@users.noreply.github.com>
2021-03-22 11:19:03 +0100

    Rajout des annonces aleatoire sur la page d'accueil

--- Code/src/controler/navigation.php
+++ Code/src/controler/navigation.php
@@ -4,21 +4,43 @@
  * @brief     This controller is designed to manage all navigation aspect (view controller view)
  * @author    Created by Pascal.BENZONANA
  * @author    Updated by Nicolas.GLASSEY
- * @version   13-APR-2020
+ * @author    Update by Yann.FANHA
+ * @version   22.03.2021
  */
 
 /**
- * @brief This function is designed to redirect the user to the home page (depending on the action received by the index)
+ * @brief Fonction qui appelle la page d'acceuil avec des annonces aleatoire a afficher
  */
-function home()
-{
+function home(){
+    require "model/annoncesManager.php";
+
+
+    //Récuperer trois annonce aléatoirement si y'en a plus que trois
+    $annonces = getAnnonces();
+
+    $nbrAnnonce = count($annonces);
+
+    //limiter a max trois annonce
+    if ($nbrAnnonce > 3){
+        $nbrAnnonceToDisplay = 3;
+    }else{
+        $nbrAnnonceToDisplay = $nbrAnnonce;
+    }
+
+    //Valeur max et min des index
+    define("MIN_INDEX", 0);
+    define("MAX_INDEX", $nbrAnnonce-1);
+
+    for ($i = 0; $i < $nbrAnnonceToDisplay; $i++){
+        $indexAnnonceToDisplay[$i] = rand(MIN_INDEX, MAX_INDEX);
+    }
+
     require "view/home.php";
 }
 
 /**
  * @brief This function is designed to inform the user that the resource requested doesn't exist (i. e. if the user modify the url manually)
  */
-function lost()
-{
+function lost(){
     require "view/lost.php";
 }
